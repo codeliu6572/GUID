71ef2fdfcbc9ae2fbf38829ef6064779dfccf501
yons <yons@yonsdeMac-Pro.local>
2015-03-24 11:29:30 +0800

    Initial Commit

---
+++ GUID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GUID
+//
+//  Created by yons on 15/3/24.
+//  Copyright (c) 2015年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GUID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GUID
+//
+//  Created by yons on 15/3/24.
+//  Copyright (c) 2015年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
